9d2d059306152aee154def90449bbc1e95c55932
Arman <uarman2215@gmail.com>
2021-01-07 15:28:50 -0500

    New Project Monster Roster

---
+++ monster-roster/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component 
+{
+  
+  constructor()
+  {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster)=>{
+          return <div key={monster.id}>{monster.name}</div>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
